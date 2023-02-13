3873727da3055f06aad07f5b93a53b5f2753b900
Louis Bailleau <louis.bailleau@free.fr>
2023-02-13 16:13:44 +0100

    fix(ci): deploy workflow crash on node setup

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
+});
+
+/** @type{import('next').NextConfig}*/
+module.exports = {
+  ...withNextra(),
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   images: {
     unoptimized: true,
   },
-});
-
-module.exports = withNextra();
+};
