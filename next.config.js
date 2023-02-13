202b5f823326f33e72b76e4adb5d2e12a528c871
Louis Bailleau <louis.bailleau@free.fr>
2023-02-13 16:08:33 +0100

    ci: 👷 configure github action for deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+});
 
-module.exports = withNextra()
+module.exports = withNextra();
