72bb5a4a0ad914b5f5e217fec34f55a05b281021
Louis Bailleau <louis.bailleau@free.fr>
2023-02-13 15:59:50 +0100

    feat: 🎉 initialize project

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Launcher Tutorials</span>,
+  project: {
+    link: 'https://github.com/bricklou/launcher-tutorials-docs',
+  },
+  chat: {
+    link: 'https://discord.gg/SAy4bVQgHD',
+  },
+  docsRepositoryBase: 'https://github.com/bricklou/launcher-tutorials-docs',
+  footer: {
+    text: 'Nextra Docs Template',
+  },
+}
+
+export default config
