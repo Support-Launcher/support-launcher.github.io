641af0e85fabce2ce64f7af3b00d7a1100dc0159
Louis Bailleau <louis.bailleau@free.fr>
2023-02-15 19:12:47 +0100

    feat: add some configuration

--- theme.config.tsx
+++ theme.config.tsx
@@ -22,6 +22,7 @@ const config: DocsThemeConfig = {
   },
   banner: {
     key: "alpha",
+    dismissible: false,
     text: (
       <p>
         ⚠ Ce site est actuellement en cours d'écriture ! Certains contenus
